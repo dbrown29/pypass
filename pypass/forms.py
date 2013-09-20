c25bfa9cc740646754fd8ead877cbeced7a2ad60
David Brown <dbrown29@gmail.com>
2013-09-20 12:23:25 -0600

    updated requirements, required updating the forms for the new wtforms

--- pypass/forms.py
+++ pypass/forms.py
@@ -1,15 +1,17 @@
-from flask.ext.wtf import Form, TextField, Required
+from flask.ext.wtf import Form
+from wtforms import TextField
+from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    username = TextField('username', validators=[Required()])
-    password = TextField('password', validators=[Required()])
-    otpass = TextField('one time password', validators=[Required()])
+    username = TextField('username', validators=[DataRequired()])
+    password = TextField('password', validators=[DataRequired()])
+    otpass = TextField('one time password', validators=[DataRequired()])
 
 class CreateUserForm(Form):
-    username = TextField('username', validators=[Required()])
-    password = TextField('password', validators=[Required()])
+    username = TextField('username', validators=[DataRequired()])
+    password = TextField('password', validators=[DataRequired()])
 
 class CreateEntryForm(Form):
-    title = TextField('title', validators=[Required()])
-    username = TextField('username', validators=[Required()])
-    password = TextField('password', validators=[Required()])
+    title = TextField('title', validators=[DataRequired()])
+    username = TextField('username', validators=[DataRequired()])
+    password = TextField('password', validators=[DataRequired()])
